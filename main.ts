cc65b99bc0575c52ac58ad525e309dab074c375a
yprazzi <72910828+yprazzi@users.noreply.github.com>
2021-05-27 09:27:21 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.AB, function () {
+    led.stopAnimation()
+    basic.showString("Le Nombre" + nbr + "en \"%\"" + nbr * 10 / 100 + "%")
+})
+let DE2 = 0
+let DE1 = 0
+let nbr = 0
+nbr = nbr + 1
+for (let index = 0; index < nbr; index++) {
+    DE1 = randint(1, 6)
+    DE2 = randint(1, 6)
+    if (DE1 == DE2) {
+    	
+    } else {
+        while (DE1 == DE2) {
+        	
+        }
+    }
+}
